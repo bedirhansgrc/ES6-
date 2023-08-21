6b1d09b6f9ded6c82d66a7de107bed9f114886aa
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-21 22:43:08 +0300

    Update es6.js

--- es6.js
+++ es6.js
@@ -1,11 +1,27 @@
-function Employee(name,age,salary){
-    this.name = name
-    this.age = age
-    this.salary = salary
-}
-Employee.prototype.showInfos = function(){
-    console.log("İsim : " + this.name +", Yaş : " + this.age + ", Maaş : " + this.salary)
+// function Employee(name,age,salary){
+//     this.name = name
+//     this.age = age
+//     this.salary = salary
+// }
+// Employee.prototype.showInfos = function(){
+//     console.log("İsim : " + this.name +", Yaş : " + this.age + ", Maaş : " + this.salary)
+// }
+
+// const emp1 = new Employee("Bedo",22,69000) 
+// emp1.showInfos()
+
+
+class Employee {
+    constructor(name,age,salary){
+        this.name = name
+        this.age = age
+        this.salary = salary
+    }
+    showInfos(){
+        console.log("İsim : " + this.name +", Yaş : " + this.age + ", Maaş : " + this.salary)
+
+    }
 }
 
-const emp1 = new Employee("Bedo",22,69000) 
-emp1.showInfos()
+const emp1 = new Employee("Bedo",22,69000)
+console.log(emp1)
