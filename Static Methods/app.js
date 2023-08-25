164f735c570d9ad991046385d484215b48e21855
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-25 19:45:11 +0300

    Update app.js

--- Static Methods/app.js
+++ Static Methods/app.js
@@ -1,9 +1,9 @@
-class Matematik{
-    static cube(x){
-        console.log(x*x*x)
+class Matematik {
+    static cube(x) {
+        console.log(x * x * x)
     }
-    sqrt(x){
-        console.log(x*x)
+    sqrt(x) {
+        console.log(x * x)
     }
 }
 const math = new Matematik()
